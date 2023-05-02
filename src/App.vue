f6fbc73152e47eed9720929e55f8ed6782571177
Abiodun Sabitu <Abiodun.Sabitu@Outlook.com>
2023-05-02 21:56:08 +0100

    project set up- first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <app-feeder></app-feeder>
+  </div>
+</template>
+<script>
+import AppFeeder from "./components/AppFeeder.vue";
+export default {
+  components: {
+    AppFeeder,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style></style>
---
+++ src/components/AppFeeder.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">{{ greeting }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Hello World",
+    };
+  },
+};
+</script>
+<style></style>
