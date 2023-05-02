8dfd2c3b0c17e052849e3b11423fc8bbc718032b
Abiodun Sabitu <Abiodun.Sabitu@Outlook.com>
2023-05-02 23:45:11 +0100

    App title created plus search bar component

--- src/components/AppFeeder.vue
+++ src/components/AppFeeder.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="">{{ greeting }}</div>
+  <div>
+    <the-header></the-header>
+    <section>
+      <weather-console></weather-console>
+    </section>
+  </div>
 </template>
 <script>
+import TheHeader from "./TheHeader.vue";
+import WeatherConsole from "./WeatherConsole.vue";
 export default {
+  components: {
+    TheHeader,
+    WeatherConsole,
+  },
   data() {
     return {
       greeting: "Hello World",
--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,7 @@
 <template>
-  <div></div>
+  <div class="container-fluid text-center py-4">
+    <h3 class="">QUICKMOSPHERE</h3>
+  </div>
 </template>
 <script>
 export default {
