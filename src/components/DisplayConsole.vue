e665ea594e3faf074ece27c3066f944c16e1fcf1
Abiodun Sabitu <Abiodun.Sabitu@Outlook.com>
2023-05-03 22:37:03 +0100

    CSS applied and logic scaled

--- src/components/DisplayConsole.vue
+++ src/components/DisplayConsole.vue
@@ -1,22 +1,32 @@
 <template>
-  <section
-    class="container col-xl-7 border border-1 border-mute d-flex justify-content-center"
-  >
-    <div class="mt-4 col border border-1 border-mute">
-      <div>
-        <h2>{{ city }}, {{ country }}</h2>
-        <h3>{{ temperature }} °C</h3>
-        <p>{{ condition }} - {{ description }}</p>
-        <img :src="'https://openweathermap.org/img/w/' + icon + '.png'" />
-        <p>Humidity: {{ humidity }}%</p>
-        <p>Min Temp: {{ minTemp }} °C</p>
-        <p>Max Temp: {{ maxTemp }} °C</p>
+  <section class="container col-xl-7 d-flex justify-content-center">
+    <div class="mt-4 col border border-1 border-mute glass p-4 shadow">
+      <div class="d-sm-flex justify-content-between gap-4">
+        <article>
+          <h2>{{ city }}, {{ country }}</h2>
+          <h3>{{ temperature }} °C</h3>
+          <p>{{ condition }} - {{ description }}</p>
+          <img
+            :src="`https://openweathermap.org/img/w/${icon}.png`"
+            :style="{ width: iconSize + 'px' }"
+          />
+        </article>
+        <article class="d-flex flex-column mt-4 fs-6 fw-normal">
+          <p>Humidity: {{ humidity }}%</p>
+          <p>Min Temp: {{ minTemp }} °C</p>
+          <p>Max Temp: {{ maxTemp }} °C</p>
+        </article>
       </div>
     </div>
   </section>
 </template>
 <script>
 export default {
+  data() {
+    return {
+      iconSize: 70,
+    };
+  },
   props: {
     city: {
       type: String,
@@ -61,4 +71,14 @@ export default {
   },
 };
 </script>
-<style></style>
+<style>
+.glass {
+  /* From https://css.glass */
+  background: rgba(255, 255, 255, 0.11);
+  border-radius: 16px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(15px);
+  -webkit-backdrop-filter: blur(2.8px);
+  border: 1px solid rgba(255, 255, 255, 0.31);
+}
+</style>
